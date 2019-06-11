e5598d1199b2304e7eac53a08c3323479775c0d7
Pradel Léo <pradel.leo@gmail.com>
2019-06-11 21:24:33 +0200

    Update doczrc.js

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,7 @@
 export default {
+  title: 'react-blockstack-button',
+  description: 'React component to display a login with blockstack button',
+  codeSandbox: false,
   typescript: true,
   notUseSpecifiers: true,
   filterComponents: files => files.filter(file => /([^d]\.tsx?)$/.test(file)),
